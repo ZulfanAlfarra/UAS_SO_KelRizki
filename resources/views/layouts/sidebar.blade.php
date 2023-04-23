3bf28a1542912b3018ee436d566bf929dacc365f
ZulfanAlfarra <zulfansyahidanalfarra.91@gmail.com>
2023-04-23 08:59:58 +0700

    tes

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- JQuery -->
+    <script src="{{ asset('js/jquery-3.6.4.min.js')}}"></script>
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+
+    <!-- Bootstrap External -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+
+    <!-- Private External -->
+    <link rel="stylesheet" href="{{ asset('css/css.css') }}">
+
+    <!-- animate.css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+
+    <!-- printThis() --> 
+    <script src="{{ asset('js/printThis.js') }}"></script>
+    
+    @yield('head')
+
+    <title>Document</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
